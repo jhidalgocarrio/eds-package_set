af137540ba6ed2714aff72e90b27cf4e7a546de8
Javier Hidalgo-Carrió <havyhidalgo@gmail.com>
2022-04-07 18:03:16 +0200

    init: change

--- init.rb
+++ init.rb
@@ -1,18 +1,17 @@
-configuration_option('ROCK_SELECTED_FLAVOR', 'string',
+configuration_option('EDS_SELECTED_FLAVOR', 'string',
     :default => 'master',
-    :possible_values => ['stable', 'master'],
+    :possible_values => ['ceres', 'master'],
     :doc => [
-        "Which flavor of Rock do you want to use ?",
-        "Stay with the default ('master') if you want to use Rock on the most recent",
-        "distributions (Ubuntu 16.04 and later). Use 'stable' only for ",
-        "now officially unsupported distributions (Ubuntu 14.04)"])
+        "Which flavor of EDS do you want to use ?",
+        "Stay with the default ('master') if you want to use EDS with DSO backend (recommended)",
+        "otherwise select ('ceres')"])
 
-if Rock.in_release? && !Autoproj.config.has_value_for?('ROCK_SELECTED_FLAVOR')
-    Autoproj.config.set 'ROCK_SELECTED_FLAVOR', 'stable', true
+if !Autoproj.config.has_value_for?('EDS_SELECTED_FLAVOR')
+    Autoproj.config.set 'EDS_SELECTED_FLAVOR', 'stable', true
 end
 
-Rock.flavors.select_current_flavor_by_name(
-    ENV['ROCK_FORCE_FLAVOR'] || Autoproj.config.get('ROCK_SELECTED_FLAVOR'))
+#Rock.flavors.select_current_flavor_by_name(
+#    ENV['ROCK_FORCE_FLAVOR'] || Autoproj.config.get('ROCK_SELECTED_FLAVOR'))
 
-current_flavor = Rock.flavors.current_flavor
+#current_flavor = Rock.flavors.current_flavor
 
