6ba6eef89cd8cdb121993c637ec95b0318258ed1
Javier Hidalgo-Carrió <havyhidalgo@gmail.com>
2022-04-08 11:09:38 +0200

    init: testing

--- init.rb
+++ init.rb
@@ -1,8 +1,18 @@
 # Orocos Specific ignore rules
 #
-Rock.flavors.define 'stable'
-Rock.flavors.alias 'stable', 'next'
-Rock.flavors.define 'master', :implicit => true
+#
+
+module Rock
+
+    def self.setup_eds_configuration_options(ws: Autoproj.workspace)
+        ws.config.declare 'EDS_SELECTED_FLAVOR', 'string',
+            default: 'master',
+            possible_values: ['ceres', 'master'],
+            doc: [ "Do you want to activate python?" ]
+    end
+end
+
+Rock.setup_eds_configuration_options
 
 configuration_option('EDS_SELECTED_FLAVOR', 'string',
     :default => 'master',
@@ -16,130 +26,4 @@ if !Autoproj.config.has_value_for?('EDS_SELECTED_FLAVOR')
     Autoproj.config.set 'EDS_SELECTED_FLAVOR', 'stable', true
 end
 
-
-configuration_option('ROCK_SELECTED_FLAVOR', 'string',
-    :default => 'master',
-    :possible_values => ['stable', 'master'],
-    :doc => [
-        "Which flavor of Rock do you want to use ?",
-        "Stay with the default ('master') if you want to use Rock on the most recent",
-        "distributions (Ubuntu 16.04 and later). Use 'stable' only for ",
-        "now officially unsupported distributions (Ubuntu 14.04)"])
-
-if Rock.in_release? && !Autoproj.config.has_value_for?('ROCK_SELECTED_FLAVOR')
-    Autoproj.config.set 'ROCK_SELECTED_FLAVOR', 'stable', true
-end
-
-Rock.flavors.select_current_flavor_by_name(
-    ENV['ROCK_FORCE_FLAVOR'] || Autoproj.config.get('ROCK_SELECTED_FLAVOR'))
-
-current_flavor = Rock.flavors.current_flavor
-
-#This check is needed because the overrides file will override the FLAVOR selection.
-#Furthermore a selection != stable can cause a inconsistent layout (cause by in_flavor system in the package_sets)
-if Rock.in_release? && current_flavor.branch != "stable" 
-    if ENV['ROCK_RC'] == '1'
-        Autoproj.warn ""
-        Autoproj.warn "Found a release file and the flavor is not master"
-        Autoproj.warn "This would usually be an error, but since ROCK_RC is set to 1,"
-        Autoproj.warn "it is assumed that you're preparing a release candidate"
-        Autoproj.warn ""
-    else
-        Autoproj.error ""
-        Autoproj.error "You selected the flavor '#{current_flavor.branch}' but '#{File.join(Autoproj.root_dir,"autoproj", "overrides.d", "25-release.yml")}' exists."
-        Autoproj.error "This means you are on a release; either unselect the release by calling 'rock-release switch master'"
-        Autoproj.error "or call 'autoproj reconfigure' and select the FLAVOR 'stable'"
-        exit 1
-    end
-end
-
-Autoproj.config.set('ROCK_SELECTED_FLAVOR', current_flavor.name, true)
-Autoproj.config.set('ROCK_FLAVOR', current_flavor.branch, true)
-Autoproj.config.set('ROCK_BRANCH', current_flavor.branch, true)
-
-if current_flavor.name != 'master' && Autoproj::PackageSet.respond_to?(:add_source_file)
-    Autoproj::PackageSet.add_source_file "source-stable.yml"
-end
-
-if Autoproj.respond_to?(:workspace) # autoproj 2.0
-    Rock.setup_cxx_support(Autoproj.workspace.os_package_resolver, Autoproj.config)
-else
-    Rock.setup_cxx_support(Autoproj.osdeps, Autoproj.config)
-end
-
-def enabled_flavor_system
-    Rock.flavors.register_flavored_package_set(Autoproj.current_package_set)
-end
-
-def in_flavor(*flavors, &block)
-    Rock.flavors.in_flavor(*flavors, &block)
-end
-
-def only_in_flavor(*flavors, &block)
-    Rock.flavors.only_in_flavor(*flavors, &block)
-end
-
-def flavor_defined?(flavor_name)
-    Rock.flavors.has_flavor?(flavor_name)
-end
-
-def package_in_flavor?(pkg, flavor_name)
-    Rock.flavors.package_in_flavor?(pkg, flavor_name)
-end
-
-def add_packages_to_flavors(mappings)
-    Rock.flavors.add_packages_to_flavors(Autoproj.current_package_set, mappings)
-end
-
-def remove_packages_from_flavors(mappings)
-    Rock.flavors.remove_packages_from_flavors(Autoproj.current_package_set, mappings)
-end
-
-# Defines a bundle package in the installation
-#
-# So far, bundles are mostly Ruby packages
-def bundle_package(*args, &block)
-    ruby_package(*args) do |pkg|
-        if block_given?
-            pkg.instance_eval(&block)
-        end
-    end
-end
-
-# Verify that a valid ruby version i used
-if defined?(RUBY_VERSION) && (RUBY_VERSION.to_f < 2.0) && Autoproj.config.get('ROCK_FLAVOR') == 'master'
-    Autoproj.error "Ruby below 2.0 is not supported by Rock anymore."
-    Autoproj.error "Please re-bootstrap your installation."
-    Autoproj.error "We recommend ruby 2.1 expect for Ubuntu 14.04 where ruby 2.0 should be used."
-    Autoproj.error "You have also the option to switch to the 'stable' flavor by running 'autoproj reconfigure'"
-    Autoproj.error "or use the rock-15.05 release by first switching to 'stable' and then running 'rock-release switch rock-15.05'."
-    Autoproj.error "If you need to check the state of this installation (to make sure everyting is pushed) you could run"
-    Autoproj.error "'ROCK_IGNORE_RUBY_VERSION=1 autoproj status'."
-    Autoproj.error ""
-    Autoproj.error "See http://rock-robotics.org/documentation/installation.html for more information regarding bootstrapping."
-    if !ENV['ROCK_IGNORE_RUBY_VERSION']
-        exit 1
-    end
-end
-
-
-# rtt doesn't support mqueue on Mac OS X
-if Autobuild.macos?
-    Autobuild::Orogen.transports.delete("mqueue")
-end
-
-Rock.setup_python_configuration_options
-
-Autoproj.config.declare 'syskit_use_bundles', 'boolean',
-    default: true,
-    short_doc: 'whether Syskit systems should load Rock\'s bundle plugin',
-    doc: ['Whether Syskit bundles should be loading the Rock bundle system',
-          'The Rock bundle system may be loaded in Syskit, to apply the ROCK_BUNDLE_*',
-          'environment variables to the Syskit apps. This is fragile, we recommend',
-          'setting it to OFF and explicitely using Syskit\'s Roby.app.register_app',
-          'mechanism. It is ON by default for backward compatibility reasons']
-
-unless Autoproj.config.has_value_for?('syskit_use_bundles')
-    Autoproj.config.set 'syskit_use_bundles', true, true
-end
 
