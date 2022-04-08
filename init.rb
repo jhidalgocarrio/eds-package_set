f8e1cf8c6b80d28ec7a855823e0449614d15f5dd
Javier Hidalgo-Carrió <havyhidalgo@gmail.com>
2022-04-08 10:56:58 +0200

    init: testing

--- init.rb
+++ init.rb
@@ -4,6 +4,19 @@ Rock.flavors.define 'stable'
 Rock.flavors.alias 'stable', 'next'
 Rock.flavors.define 'master', :implicit => true
 
+configuration_option('EDS_SELECTED_FLAVOR', 'string',
+    :default => 'master',
+    :possible_values => ['ceres', 'master'],
+    :doc => [
+        "Which flavor of EDS do you want to use ?",
+        "Stay with the default ('master') if you want to use EDS with DSO backend (recommended)",
+        "otherwise select ('ceres')"])
+
+if !Autoproj.config.has_value_for?('EDS_SELECTED_FLAVOR')
+    Autoproj.config.set 'EDS_SELECTED_FLAVOR', 'stable', true
+end
+
+
 configuration_option('ROCK_SELECTED_FLAVOR', 'string',
     :default => 'master',
     :possible_values => ['stable', 'master'],
