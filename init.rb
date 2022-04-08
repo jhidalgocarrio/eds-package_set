7defa47419ec572738aeeaf9422e780ceda1d899
Javier Hidalgo-Carrió <havyhidalgo@gmail.com>
2022-04-08 13:13:49 +0200

    init: test

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 # Orocos Specific ignore rules
 #
 #
-Autoproj.env_set 'EDS_BRANCH', 'master'
+#Autoproj.env_set 'EDS_BRANCH', 'master'
 
-configuration_option('EDS_SELECTED_BRANCH', 'string',
+configuration_option('EDS_BRANCH', 'string',
     :default => 'master',
     :possible_values => ['ceres', 'master'],
     :doc => [
@@ -11,8 +11,9 @@ configuration_option('EDS_SELECTED_BRANCH', 'string',
         "Stay with the default ('master') if you want to use EDS with DSO backend",
         "otherwise select ('ceres')"])
 
-Autoproj.env_set 'EDS_BRANCH', Autoproj.config.get('EDS_SELECTED_BRANCH')
+#Autoproj.env_set 'EDS_BRANCH', Autoproj.config.get('EDS_BRANCH')
+Autoproj.config.set 'EDS_BRANCH', Autoproj.config.get('EDS_BRANCH')
 
-pp Autoproj.env['EDS_BRANCH']
+#pp "SELECTED EDS: " + Autoproj.env['EDS_BRANCH']
 
 
