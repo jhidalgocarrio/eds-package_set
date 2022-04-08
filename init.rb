dc82f16a12d6f179c11efaf7c2867a6f2e78cd4e
Javier Hidalgo-Carrió <havyhidalgo@gmail.com>
2022-04-08 11:59:05 +0200

    init: testing

--- init.rb
+++ init.rb
@@ -1,29 +1,18 @@
 # Orocos Specific ignore rules
 #
 #
+Autoproj.env_set 'EDS_BRANCH', 'master'
 
-module Rock
-
-    def self.setup_eds_configuration_options(ws: Autoproj.workspace)
-        ws.config.declare 'EDS_SELECTED_FLAVOR', 'string',
-            default: 'master',
-            possible_values: ['ceres', 'master'],
-            doc: [ "Do you want to activate python?" ]
-    end
-end
-
-Rock.setup_eds_configuration_options
-
-configuration_option('EDS_SELECTED_FLAVOR', 'string',
+configuration_option('EDS_SELECTED_BRANCH', 'string',
     :default => 'master',
     :possible_values => ['ceres', 'master'],
     :doc => [
         "Which flavor of EDS do you want to use ?",
-        "Stay with the default ('master') if you want to use EDS with DSO backend (recommended)",
+        "Stay with the default ('master') if you want to use EDS with DSO backend",
         "otherwise select ('ceres')"])
 
-if !Autoproj.config.has_value_for?('EDS_SELECTED_FLAVOR')
-    Autoproj.config.set 'EDS_SELECTED_FLAVOR', 'stable', true
-end
+Autoproj.env_set 'EDS_BRANCH', Autoproj.config.get('EDS_SELECTED_BRANCH')
+
+pp Autoproj.env['EDS_BRANCH']
 
 
